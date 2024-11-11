f1faf1c722e8b24e845951ac27b94d7df8afbac1
pratikjc21 <patil.pratik@outlook.com>
2024-11-11 23:59:43 +0530

    user input

---
+++ Nov/ nov_10112024_basic/Lab_005_Python_userinput.py
@@ -0,0 +1,18 @@
+#Input
+Num_1= int(input("Enter num 1: "))
+Num_2= int(input("Enter num 2: "))
+Num_3= int(input("Enter num 3: "))
+
+#Logic
+Sum= int(Num_1+Num_2+Num_3)
+Sub= int(Num_1-Num_2-Num_3)
+Mul= int(Num_1*Num_2*Num_3)
+Div= float(Num_1/Num_2/Num_3)
+
+#Output
+print("Sum of 3 numbers is",Sum)
+print("Sub of 3 numbers is",Sub)
+print("Mul of 3 numbers is",Mul)
+print("Div of 3 numbers is",Div)
+
+
