68e845638d39c0fcab7f4ff1755298e2f16a2278
pratikjc21 <patil.pratik@outlook.com>
2024-11-12 10:46:11 +0530

    Quot and rem

---
+++ Nov/ nov_10112024_basic/Lab006_Python_Divlogic_Rem_Quot.py
@@ -0,0 +1,14 @@
+# Input
+
+Num_1 = float(input("Enter num 1: "))
+Num_2 = float(input("Enter num 2: "))
+
+# Logic
+Div = float(Num_1 / Num_2)
+Quotient = int(Num_1 // Num_2)
+Remainder = int(Num_1 % Num_2)
+
+# Output
+print("Div of 2 numbers is", Div)
+print("Rem of 2 numbers is", Remainder)
+print("Quot of 2 numbers is", Quotient)
